c4670badf6649b13e6376c57d339f12412ac8f90
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-11-23 14:51:11 -0500

    update that damn embed once again

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,15 +6,15 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta property="og:title" content="Rehosted SH1mmer" />
-          <meta property="og:description" content="SH1mmer: Downloads" />
+          <meta property="og:title" content="Darkn's File Host" />
+          <meta property="og:description" content="Another File Host" />
           <meta property="og:url" content="https://dl.osu.bio" />
           <meta property="og:type" content="website" />
-          <meta property="og:image" content="https://osu.bio/img/file.png" />
-          <meta property="og:image:alt" content="SH1mmer" />
+          <meta property="og:image" content="https://osu.bio/img/download.webp" />
+          <meta property="og:image:alt" content="Files" />
           <meta property="og:image:width" content="512" />
           <meta property="og:image:height" content="512" />
-          <meta property="og:site_name" content="SH1mmer" />
+          <meta property="og:site_name" content="Darkn" />
           <link rel="icon" href="/favicon.ico" />  
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
