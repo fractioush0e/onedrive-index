1fd298eaf57876b133ae47ed26f00c2757269045
Darkn <73033672+NotDarkn@users.noreply.github.com>
2024-08-13 19:06:50 -0400

    chore(config): i'm sowwy

--- next.config.js
+++ next.config.js
@@ -5,5 +5,4 @@ module.exports = {
   reactStrictMode: true,
   // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
   trailingSlash: true,
-  images.unoptimized = true,
 }
